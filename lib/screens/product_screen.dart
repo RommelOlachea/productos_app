10fd4e5a3609c95a4db8a14cba6afd195989447f
RommelOlachea <msc.rommel.olachea@gmail.com>
2023-02-15 21:46:08 -0700

    Actualizo la url del imagen en la base de datos

--- lib/screens/product_screen.dart
+++ lib/screens/product_screen.dart
@@ -104,16 +104,24 @@ class _ProductScreenBody extends StatelessWidget {
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
       floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.save_alt_outlined),
-        onPressed: () async {
-          if (!productForm.isValidForm()) return;
+        child: productService.isSaving
+            ? CircularProgressIndicator(
+                color: Colors.white,
+              )
+            : Icon(Icons.save_alt_outlined),
+        onPressed: productService.isSaving
+            ? null
+            : () async {
+                if (!productForm.isValidForm()) return;
 
-          final String? imageUrl = await productService.uploadImage();
+                final String? imageUrl = await productService.uploadImage();
 
-          print(imageUrl);
+                if (imageUrl != null) {
+                  productForm.product.picture = imageUrl;
+                }
 
-          await productService.saveOrCreateProduct(productForm.product);
-        },
+                await productService.saveOrCreateProduct(productForm.product);
+              },
       ),
     );
   }
