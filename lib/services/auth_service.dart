9ef2dd811b99a5a1166c7dd21381657e126e4321
Rommel Olachea Cota <97263258+RommelOlachea@users.noreply.github.com>
2023-02-17 12:56:20 -0700

    Se creo el metodo para iniciar sesion en el authProvider

---
+++ lib/services/auth_service.dart
@@ -0,0 +1,32 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class AuthService extends ChangeNotifier {
+  final String _baseUrl = 'identitytoolkit.googleapis.com';
+  //_firebaseToken, es el token de firebase para acccesar a si API
+  final String _firebaseToken = 'AIzaSyAIpNTWAcemkhEr3c-ou_Wq_pwWLNUMq9o';
+
+  Future<String?> createUser(String email, String password) async {
+    //Creamos el mapa que se enviara como data en el metodo post
+    final Map<String, dynamic> authData = {
+      'email': email,
+      'password': password
+    };
+
+    //el mapa que contiene el key, es el argumento enviado, este caso el apikey token
+    final url = Uri.https(
+        _baseUrl, '/v1/accounts:signInWithPassword', {'key': _firebaseToken});
+
+    final resp = await http.post(url, body: json.encode(authData));
+    final Map<String, dynamic> decodeResp = json.decode(resp.body);
+
+    print(decodeResp);
+  }
+}
+
+/*liga con la documentacion sobre la autenticacion con email y password en firebase
+https://firebase.google.com/docs/reference/rest/auth?hl=es-419#section-sign-in-email-password
+configuracion en el ep. 247
+*/
