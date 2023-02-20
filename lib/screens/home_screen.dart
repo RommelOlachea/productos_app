0cacc0d76b5a09ececb4eac4a35a619771f47f99
RommelOlachea <msc.rommel.olachea@gmail.com>
2023-02-19 21:36:09 -0700

    Cerar sesion desde el HomeScreen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -12,13 +12,20 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final productsService = Provider.of<ProductsService>(context);
-
+    final authService = Provider.of<AuthService>(context, listen: false);
     /*Preguntamos si esta en carga de articulos o finalizo*/
     if (productsService.isLoading) return LoadingScreen();
 
     return Scaffold(
       appBar: AppBar(
         title: const Center(child: Text('Productos')),
+        leading: IconButton(
+          onPressed: () async {
+            authService.logOut();
+            Navigator.pushReplacementNamed(context, LoginScreen.routeName);
+          },
+          icon: Icon(Icons.login_outlined),
+        ),
       ),
       body: ListView.builder(
           itemCount: productsService.products.length,
